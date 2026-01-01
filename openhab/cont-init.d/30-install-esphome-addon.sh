60aa8157a544163b691932d6ac8f9f505c91f6cf
pappnu <pppskraga@gmail.com>
2026-01-01 10:41:02 +0200

    Install ESPHome binding during container initialisation

---
+++ openhab/cont-init.d/30-install-esphome-addon.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# https://community.openhab.org/t/user-creation-management-on-initial-start-of-openhab-on-docker-kubernetes/141654/12
+# Ensure that the openhab user is used as otherwise some files receive wrong ownership and the main instance of
+# openHAB fails to start at the end to the Docker entrypoint
+/bin/bash -c "su-exec openhab /openhab/runtime/bin/start"
+sleep 40
+# Explicitly install ESPHome binding for the Native API, since it sometimes doesn't survive Docker container updates
+sshpass -p habopen ssh -o StrictHostKeyChecking=no -p 8101 openhab@localhost "openhab:addons install marketplace:146849"
+/bin/bash -c "su-exec openhab /openhab/runtime/bin/stop"
+sleep 40
+#chown -R openhab:openhab "${OPENHAB_HOME}"
